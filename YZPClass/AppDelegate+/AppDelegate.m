45ecac6bf64e1ac7d73b8d542473f575e98ddf74
Andy <>
2019-03-29 14:27:37 +0800

    no message

--- YZPClass/AppDelegate+/AppDelegate.m
+++ YZPClass/AppDelegate+/AppDelegate.m
@@ -14,11 +14,6 @@
  */
 
 #import "AppDelegate.h"
-#import "MainTabBarController.h"
-#import "NetWork.h"
-
-#import "AdvertiseHelper.h"
-#import "GuidePagesHelper.h"
 
 @interface AppDelegate ()
 
@@ -31,7 +26,7 @@
     
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[MainTabBarController alloc] init];
+    self.window.rootViewController = [[UIViewController alloc] init];
 
     
     
@@ -47,27 +42,27 @@
     return YES;
 }
 
-#pragma mark 启动广告
-- (void)setupAdveriseView{
-    // TODO 请求广告接口 获取广告图片
-    
-    //现在了一些固定的图片url代替
-    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/9213b07eca80653846dc8fab97dda144ad348257.jpg", @"http://pic.paopaoche.net/up/2012-2/20122220201612322865.png", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://www.mangowed.com/uploads/allimg/130410/1-130410215449417.jpg"];
-    
-    [AdvertiseHelper showAdvertiserView:imageArray];
-}
-
-#pragma mark 引导页
-- (void)setupGuidePage
-{
-//    if ([ReadForLocation(IsNotFirstLaunch_Key) boolValue])
-//    {
-//        return;
-//    }
-//    WriteForLocation(@(YES), IsNotFirstLaunch_Key);
-    NSArray *images = @[@"introductoryPage1",@"introductoryPage2",@"introductoryPage3",@"introductoryPage4"];
-    [GuidePagesHelper showGuidePagesView:images];
-}
+//#pragma mark 启动广告
+//- (void)setupAdveriseView{
+//    // TODO 请求广告接口 获取广告图片
+//
+//    //现在了一些固定的图片url代替
+//    NSArray *imageArray = @[@"http://imgsrc.baidu.com/forum/pic/item/9213b07eca80653846dc8fab97dda144ad348257.jpg", @"http://pic.paopaoche.net/up/2012-2/20122220201612322865.png", @"http://img5.pcpop.com/ArticleImages/picshow/0x0/20110801/2011080114495843125.jpg", @"http://www.mangowed.com/uploads/allimg/130410/1-130410215449417.jpg"];
+//
+//    [AdvertiseHelper showAdvertiserView:imageArray];
+//}
+//
+//#pragma mark 引导页
+//- (void)setupGuidePage
+//{
+////    if ([ReadForLocation(IsNotFirstLaunch_Key) boolValue])
+////    {
+////        return;
+////    }
+////    WriteForLocation(@(YES), IsNotFirstLaunch_Key);
+//    NSArray *images = @[@"introductoryPage1",@"introductoryPage2",@"introductoryPage3",@"introductoryPage4"];
+//    [GuidePagesHelper showGuidePagesView:images];
+//}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
